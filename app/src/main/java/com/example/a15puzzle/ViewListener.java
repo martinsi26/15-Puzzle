1b374919a45c3425e1d627d0e9ba0f9c8e118c1d
martinsi26 <112525187+martinsi26@users.noreply.github.com>
2022-09-28 18:48:58 -0700

    Comments added

--- app/src/main/java/com/example/a15puzzle/ViewListener.java
+++ app/src/main/java/com/example/a15puzzle/ViewListener.java
@@ -6,35 +6,57 @@ import android.widget.Button;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * 15 Puzzle: creates a 4v4 puzzle game in which you order the numbers from 1 - 15
+ *
+ * @author **** Simon Martin ****
+ * @version **** 9/26/22 ****
+ */
+
 public class ViewListener implements View.OnClickListener{
 
-    private Button[][] gameButtons = new Button[4][4];
-    private ArrayList<Integer> nums;
-    private ArrayList<Integer> numbers;
-    private ArrayList<Integer> adjacent;
-    private int blankX;
-    private int blankY;
-    private int count;
+    private Button[][] gameButtons; // array of buttons for each of the squares
+    private ArrayList<Integer> nums; // arraylist of numbers for the 15 different numbered squares
+    private ArrayList<Integer> numbers; // arraylist of numbers to check if the puzzle is solvable
+    private ArrayList<Integer> adjacent; // arraylist of all numbers adjacent to the open square
+    private int blankX; // x value of the open square
+    private int blankY; // y value of the open square
+    private int count; // counter for checking if the puzzle is solvable
 
+    /**
+     * constructor that displays all the buttons on the screen in a random order
+     * @param _gameButtons the 15 buttons on the screen
+     */
     public ViewListener(Button[][] _gameButtons) {
+        gameButtons = new Button[4][4];
         count = 0;
         nums = new ArrayList<>();
         numbers = new ArrayList<>();
+
+        // adding all numbers to the array 1 - 16
         for(int i = 1; i <= 16; i++) {
             nums.add(i);
         }
+
+        // adding all the buttons
         for(int i = 0; i < _gameButtons.length; i++) {
             gameButtons[i] = _gameButtons[i];
         }
 
+        // randomizing the numbers order so the button numbers can be random
         Collections.shuffle(nums);
         numbers.clear();
+
+        // adding the randomized numbers to a new number arraylist
         for(int i = 0; i < nums.size(); i++) {
             if(nums.get(i) != 0 && nums.get(i) != 16) {
                 numbers.add(nums.get(i));
             }
         }
 
+        // check if the current numbers are suitable for a winnable situation
+        // if not it randomizes the numbers and checks again until there is a
+        // winnable situation
         while(!checkSuitableNumbers()) {
             Collections.shuffle(nums);
             numbers.clear();
@@ -45,6 +67,7 @@ public class ViewListener implements View.OnClickListener{
             }
         }
 
+        // changes the text of the buttons to the new numbers
         int counter = 0;
         for(int row = 0; row < gameButtons.length; row++) {
             for(int col = 0; col < gameButtons[row].length; col++) {
@@ -61,6 +84,10 @@ public class ViewListener implements View.OnClickListener{
         checkCorrectPosition();
     }
 
+    /**
+     * Checks for the squares around the empty square and adds the numbers to an array
+     *
+     */
     public void checkEmptyAdjacent() {
         adjacent = new ArrayList<>();
         for(int row = 0; row < gameButtons.length; row++) {
@@ -85,9 +112,17 @@ public class ViewListener implements View.OnClickListener{
         }
     }
 
+    /**
+     * Using the x and y values of the square and determines if the square pressed
+     * should switch with the empty square and moves it
+     * @param screenX x value of the button clicked
+     * @param screenY y value of the button clicked
+     */
     public void changeNumbers(float screenX, float screenY) {
         int x = 0;
         int y = 0;
+
+        // sets the x and y values to integer values that can be used in the 2d array of buttons
         if(screenY == 100) {
             y = 0;
         } else if(screenY == 99) {
@@ -106,6 +141,9 @@ public class ViewListener implements View.OnClickListener{
         } else if(screenX == 884.0) {
             x = 3;
         }
+
+        // checks if the button is apart of the adjacent arraylist meaning it is next to the button
+        // so that the button and the empty square can switch places
         for(int i = 0; i < adjacent.size(); i++) {
             if (gameButtons[y][x].getText() != "") {
                 if (Integer.parseInt(gameButtons[y][x].getText().toString()) == adjacent.get(i)) {
@@ -119,8 +157,13 @@ public class ViewListener implements View.OnClickListener{
         }
     }
 
+    /**
+     * Checks if the position of the button is correct so the color can change
+     */
     public void checkCorrectPosition() {
         int color = 0xFFFFA500;
+
+        // if the number is in the right position the color is changed to orange
         for(int i = 0; i < 4; i++) {
             if (gameButtons[0][i].getText().equals(String.valueOf((i + 1)))) {
                 gameButtons[0][i].setBackgroundColor(color);
@@ -137,6 +180,10 @@ public class ViewListener implements View.OnClickListener{
         }
     }
 
+    /**
+     * Checks if the solution is winnable by counting
+     * @return true if the solution is winnable
+     */
     public boolean checkSuitableNumbers() {
         count = 0;
         for(int i = 0; i < numbers.size(); i++) {
@@ -152,8 +199,16 @@ public class ViewListener implements View.OnClickListener{
         return false;
     }
 
+    /**
+     * common onClick method
+     * @param view the view of the screen
+     */
     @Override
     public void onClick(View view) {
+
+        // checks if any of the number buttons are clicked so they can possibly change with
+        // the empty button, otherwise if the newGame button is clicked the buttons are
+        // randomized again
         if(view.getId() != R.id.newGame) {
             checkEmptyAdjacent();
             changeNumbers(view.getX(), view.getAlpha());
@@ -165,6 +220,8 @@ public class ViewListener implements View.OnClickListener{
                     numbers.add(nums.get(i));
                 }
             }
+
+            // checks for the suitability with the newly changed button layout
             while(!checkSuitableNumbers()) {
                 Collections.shuffle(nums);
                 numbers.clear();
@@ -174,6 +231,9 @@ public class ViewListener implements View.OnClickListener{
                     }
                 }
             }
+
+            // changes the color of all the buttons to be blue and make the empty button be
+            // blank then changes the colors if they are in the correct position
             int counter = 1;
             for(int row = 0; row < gameButtons.length; row++) {
                 for(int col = 0; col < gameButtons[row].length; col++) {
